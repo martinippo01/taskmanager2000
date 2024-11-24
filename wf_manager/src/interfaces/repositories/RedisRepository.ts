a0015f94ff5b34ffe65f0747cbbea5b9461fd9c8
martinippo01 <martinippo@gmail.com>
2024-11-24 19:08:01 -0300

    Add redis repository for connection

---
+++ wf_manager/src/interfaces/repositories/RedisRepository.ts
@@ -0,0 +1,13 @@
+export interface RedisRepository {
+  get(prefix: string, key: string): Promise<string | null>;
+  set(prefix: string, key: string, value: string): Promise<void>;
+  delete(prefix: string, key: string): Promise<void>;
+  setWithExpiry(
+    prefix: string,
+    key: string,
+    value: string,
+    expiry: number,
+  ): Promise<void>;
+}
+
+export const RedisRepository = Symbol('RedisRepository');
---
+++ wf_manager/src/repositories/RedisRepositoryImpl.ts
@@ -0,0 +1,53 @@
+import { FactoryProvider } from '@nestjs/common';
+import { Redis } from 'ioredis';
+
+import { Inject, Injectable, OnModuleDestroy } from '@nestjs/common';
+
+import { RedisRepository } from '@interfaces/repositories/RedisRepository';
+
+export const redisClientFactory: FactoryProvider<Redis> = {
+  provide: 'RedisClient',
+  useFactory: () => {
+    const redisInstance = new Redis({
+      host: process.env.REDIS_HOST,
+      port: +process.env.REDIS_PORT,
+    });
+
+    redisInstance.on('error', (e) => {
+      throw new Error(`Redis connection failed: ${e}`);
+    });
+
+    return redisInstance;
+  },
+  inject: [],
+};
+
+@Injectable()
+export class RedisRepositoryImpl implements OnModuleDestroy, RedisRepository {
+  constructor(@Inject('RedisClient') private readonly redisClient: Redis) {}
+
+  onModuleDestroy(): void {
+    this.redisClient.disconnect();
+  }
+
+  async get(prefix: string, key: string): Promise<string | null> {
+    return this.redisClient.get(`${prefix}:${key}`);
+  }
+
+  async set(prefix: string, key: string, value: string): Promise<void> {
+    await this.redisClient.set(`${prefix}:${key}`, value);
+  }
+
+  async delete(prefix: string, key: string): Promise<void> {
+    await this.redisClient.del(`${prefix}:${key}`);
+  }
+
+  async setWithExpiry(
+    prefix: string,
+    key: string,
+    value: string,
+    expiry: number,
+  ): Promise<void> {
+    await this.redisClient.set(`${prefix}:${key}`, value, 'EX', expiry);
+  }
+}
