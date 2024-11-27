27cde10f70fa37477e2a1486e8580eed4c03f3c0
Axel Facundo Preiti Tasat <apreititasat@itba.edu.ar>
2024-11-27 17:06:26 -0300

    chore: organize type order

--- shared/lib/WorkflowPlan.ts
+++ shared/lib/WorkflowPlan.ts
@@ -1,9 +1,3 @@
-export type Step = {
-  name: string; // The name of the step
-  task: string; // The task of the step
-  params: Param[]; // The parameters of the step. The key is the name of the parameter and the value is the type of the parameter
-};
-
 export type Param = {
   name: string;
   type: string;
@@ -16,6 +10,12 @@ export type Param = {
       constant?: boolean;
     }
 ); // The parameters of the step. The key is the name of the parameter and the value is the type of the parameter
+
+export type Step = {
+  name: string; // The name of the step
+  task: string; // The task of the step
+  params: Param[]; // The parameters of the step. The key is the name of the parameter and the value is the type of the parameter
+};
 
 export type Plan = {
   steps: Step[]; // The steps of the plan
