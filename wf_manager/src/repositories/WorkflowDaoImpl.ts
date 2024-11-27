c1912a4fe0f34400c951b1fde6efbd3c5ce21b34
martinippo01 <martinippo@gmail.com>
2024-11-27 00:44:23 -0300

    logging repositury

--- wf_manager/src/repositories/WorkflowDaoImpl.ts
+++ wf_manager/src/repositories/WorkflowDaoImpl.ts
@@ -27,36 +27,50 @@ class WorkflowDaoImpl implements WorkflowDao {
     name: string,
     version: string,
   ): Promise<WorkflowMetadata | null> {
+    this.LOGGER.debug(
+      `Fetching workflow metadata for ${name} version ${version}`,
+    );
     if (version === 'latest' || version === null || version === undefined) {
       version = await this.getLatestVersion(name);
     }
     let res = await this.redisRepository.get(
       workflowMetadataKey(name, version),
     );
-    return JSON.parse(res);
+    this.LOGGER.debug(
+      `Fetched workflow metadata for ${name} version ${version}`,
+    );
+    return res ? JSON.parse(res) : null;
   }
 
   async getWorkflowPlan(name: string, version: string): Promise<Plan | null> {
+    this.LOGGER.debug(`Fetching workflow plan for ${name} version ${version}`);
     if (version === 'latest' || version === null || version === undefined) {
       version = await this.getLatestVersion(name);
     }
     let res = await this.redisRepository.get(workflowPlanKey(name, version));
-    return JSON.parse(res);
+    this.LOGGER.debug(`Fetched workflow plan for ${name} version ${version}`);
+    return res ? JSON.parse(res) : null;
   }
 
   async getWorkflow(name: string, version: string): Promise<Workflow | null> {
+    this.LOGGER.debug(`Fetching workflow for ${name} version ${version}`);
     if (version === 'latest' || version === null || version === undefined) {
       version = await this.getLatestVersion(name);
     }
     const resPlan = await this.getWorkflowPlan(name, version);
     const resMetadata = await this.getWorkflowMetadata(name, version);
     if (!resPlan || !resMetadata) {
+      this.LOGGER.warn(`Workflow not found for ${name} version ${version}`);
       return null;
     }
+    this.LOGGER.debug(`Fetched workflow for ${name} version ${version}`);
     return { ...resMetadata, plan: resPlan };
   }
 
   async createWorkflow(workflow: Workflow): Promise<boolean> {
+    this.LOGGER.debug(
+      `Creating workflow for ${workflow.name} version ${workflow.version}`,
+    );
     // TODO: consider using a MULTI here!!
     this.redisRepository.set(
       workflowLatestKey(workflow.name),
@@ -73,15 +87,20 @@ class WorkflowDaoImpl implements WorkflowDao {
       workflowMetadataKey(workflow.name, workflow.version),
       JSON.stringify(workflowMetadata),
     );
+    this.LOGGER.log(
+      `Created workflow for ${workflow.name} version ${workflow.version}`,
+    );
     throw new Error('Method not implemented.');
   }
 
   async disableWorkflow(name: string, version: string): Promise<boolean> {
+    this.LOGGER.debug(`Disabling workflow for ${name} version ${version}`);
     if (version === 'latest' || version === null || version === undefined) {
       version = await this.getLatestVersion(name);
     }
     const resMetadata = await this.getWorkflowMetadata(name, version);
     if (!resMetadata) {
+      this.LOGGER.error(`Workflow not found for ${name} version ${version}`);
       throw new Error('Workflow not found');
     }
     resMetadata.enabled = false;
@@ -89,15 +108,18 @@ class WorkflowDaoImpl implements WorkflowDao {
       workflowMetadataKey(name, version),
       JSON.stringify(resMetadata),
     );
+    this.LOGGER.log(`Disabled workflow for ${name} version ${version}`);
     throw new Error('Method not implemented.');
   }
 
   async enableWorkflow(name: string, version: string): Promise<boolean> {
+    this.LOGGER.debug(`Enabling workflow for ${name} version ${version}`);
     if (version === 'latest' || version === null || version === undefined) {
       version = await this.getLatestVersion(name);
     }
     const resMetadata = await this.getWorkflowMetadata(name, version);
     if (!resMetadata) {
+      this.LOGGER.error(`Workflow not found for ${name} version ${version}`);
       throw new Error('Workflow not found');
     }
     resMetadata.enabled = true;
@@ -105,11 +127,19 @@ class WorkflowDaoImpl implements WorkflowDao {
       workflowMetadataKey(name, version),
       JSON.stringify(resMetadata),
     );
+    this.LOGGER.log(`Enabled workflow for ${name} version ${version}`);
     throw new Error('Method not implemented.');
   }
 
-  async getLatestVersion(name: string): Promise<string | null> {
-    return this.redisRepository.get(workflowLatestKey(name));
+  async getLatestVersion(name: string): Promise<string> {
+    this.LOGGER.debug(`Fetching latest version for ${name}`);
+    const res = await this.redisRepository.get(workflowLatestKey(name));
+    if (!res) {
+      this.LOGGER.error(`Workflow not found for ${name}`);
+      throw new Error('Workflow not found');
+    }
+    this.LOGGER.debug(`Fetched latest version for ${name}`);
+    return res;
   }
 }
 
