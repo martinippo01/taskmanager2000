8b80bc41e6286f57ff34d618c2600c658a112aab
mauritobaez <mauroleanbaez@gmail.com>
2024-12-04 21:56:25 -0300

    Cosas del dao

--- orquestador/src/app.module.ts
+++ orquestador/src/app.module.ts
@@ -6,6 +6,9 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ClientsModule } from '@nestjs/microservices';
 import { WorkflowExecutionRequestController } from '@controllers/WorkflowExecutionRequestController';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { WorkflowExecutionDaoImpl } from '@repositories/WorkflowExecutionDaoImpl';
+import { WorkflowExecutionDao } from '@interfaces/repository/WorkflowExecutionDao';
 
 @Module({
   imports: [
@@ -18,8 +21,20 @@ import { WorkflowExecutionRequestController } from '@controllers/WorkflowExecuti
         useFactory: kafkaClientFactoryProvider,
       },
     ]),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT || '5432', 10),
+      username: process.env.DB_USERNAME || 'default_user',
+      password: process.env.DB_PASSWORD || 'default_password',
+      database: process.env.DB_NAME || 'default_db',
+      autoLoadEntities: true,
+      synchronize: process.env.TYPEORM_SYNC === 'true', // Set false in production
+    }),
   ],
   controllers: [WorkflowExecutionRequestController],
-  providers: [],
+  providers: [
+    { provide: WorkflowExecutionDao, useClass: WorkflowExecutionDaoImpl },
+  ],
 })
 export class AppModule {}
--- orquestador/src/interfaces/domains/WorkflowExecutionDomain.ts
+++ orquestador/src/interfaces/domains/WorkflowExecutionDomain.ts
@@ -2,7 +2,7 @@ import { WorkflowExecutionRequest } from '@shared/WorkflowExecutionRequest';
 
 export interface WorkflowExecutionDomain {
   runNewWorkflowExecution(request: WorkflowExecutionRequest): Promise<{
-    alreadyRunned: boolean; // The workflow execution has already been runned (this is a duplicate request)
+    alreadyRun: boolean; // The workflow execution has already been runned (this is a duplicate request)
     couldRun: boolean; // The workflow execution could be runned. Must be set when alreadyRunned is false
   }>;
 }
