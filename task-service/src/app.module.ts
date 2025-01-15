273935ff0c8677140a30ac411f84eca3947578ff
Axel Preiti Tasat <axel.preitit@gmail.com>
2025-01-14 21:36:29 -0300

    chore: add config validation schema

--- task-service/src/app.module.ts
+++ task-service/src/app.module.ts
@@ -1,8 +1,10 @@
+import { ConfigModuleValidationSchema } from '@configs/ConfigValidationSchema';
 import TaskAgentController from '@controllers/TaskAgentController';
 import TaskServiceDomainImpl from '@domains/TaskServiceDomainImpl';
 import { TaskServiceDomain } from '@interfaces/domains/TaskServiceDomain';
 import { TaskAgentDao } from '@interfaces/repositories/TaskAgentDao';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import TaskAgentDaoImpl from '@repositories/TaskAgentDaoImpl';
 import {
   TaskAgentDaoClient,
@@ -10,7 +12,15 @@ import {
 } from '@repositories/TaskAgentDaoProvider';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: ConfigModuleValidationSchema,
+      validationOptions: {
+        allowUnknown: true,
+        abortEarly: false,
+      },
+    }),
+  ],
   controllers: [TaskAgentController],
   providers: [
     {
