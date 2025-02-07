5ed5d008c1ff28669999e13f6de8c8931e0d0f9a
Axel Preiti Tasat <axel.preitit@gmail.com>
2025-02-07 21:59:40 +0100

    chore: add PORT env var to schema

--- scheduler/src/configs/ConfigValidationSchema.ts
+++ scheduler/src/configs/ConfigValidationSchema.ts
@@ -8,4 +8,5 @@ export const ConfigModuleValidationSchema = Joi.object({
   KAFKA_PASSWORD_SSR: Joi.string().required(),
   KAFKA_TOPIC_SSR: Joi.string().required(),
   TASK_SERVICE_URL: Joi.string().required(),
+  PORT: Joi.number().port().required(),
 });
