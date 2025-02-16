b4ec8ef2ea82034b70bcfff2c59851c35719bcb7
Axel Facundo Preiti Tasat <apreititasat@itba.edu.ar>
2025-02-16 19:41:04 +0000

    Observability: Add logger exporter

--- shared/src/Tracing.ts
+++ shared/src/Tracing.ts
@@ -1,39 +1,61 @@
-import { NodeSDK } from '@opentelemetry/sdk-node';
-import { Resource } from '@opentelemetry/resources';
-import { ATTR_SERVICE_NAME } from '@opentelemetry/semantic-conventions';
-import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
-import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
+import { NodeSDK } from "@opentelemetry/sdk-node";
+import { Resource } from "@opentelemetry/resources";
+import { ATTR_SERVICE_NAME } from "@opentelemetry/semantic-conventions";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
+import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
+import { OTLPLogExporter } from "@opentelemetry/exporter-logs-otlp-http";
+import {
+  LoggerProvider,
+  SimpleLogRecordProcessor,
+} from "@opentelemetry/sdk-logs";
 
 class OTelSDK {
   private readonly sdk: NodeSDK;
+  private readonly loggerProvider: LoggerProvider;
 
-  constructor(serviceName: string, exporterUrl: string) {
-    const exporterOptions = {
-      url: exporterUrl,
-    };
+  constructor(
+    private readonly serviceName: string,
+    private readonly loggerUrl: string,
+    private readonly traceUrl: string
+  ) {
+    const resource = new Resource({
+      [ATTR_SERVICE_NAME]: serviceName,
+    });
 
-    const traceExporter = new OTLPTraceExporter(exporterOptions);
+    // Trace exporter
+    const traceExporter = new OTLPTraceExporter({
+      url: traceUrl,
+    });
+
+    // Log exporter
+    const logExporter = new OTLPLogExporter({
+      url: loggerUrl,
+    });
+    this.loggerProvider = new LoggerProvider({ resource });
+
+    // Attach the log exporter to the logger provider
+    this.loggerProvider.addLogRecordProcessor(
+      new SimpleLogRecordProcessor(logExporter)
+    );
 
     this.sdk = new NodeSDK({
-      resource: new Resource({
-        [ATTR_SERVICE_NAME]: serviceName,
-      }),
+      resource,
       traceExporter,
       instrumentations: [
         getNodeAutoInstrumentations({
-          '@opentelemetry/instrumentation-nestjs-core': {
+          "@opentelemetry/instrumentation-nestjs-core": {
             enabled: true,
           },
         }),
       ],
     });
 
-    process.on('SIGTERM', () => {
+    process.on("SIGTERM", () => {
       this.sdk
         .shutdown()
         .then(
-          () => console.log('OTel SDK shutdown successfully'),
-          (err) => console.log('OTel SDK shutdown failed', err),
+          () => console.log("OTel SDK shutdown successfully"),
+          (err) => console.log("OTel SDK shutdown failed", err)
         )
         .finally(() => process.exit(0));
     });
@@ -41,6 +63,14 @@ class OTelSDK {
 
   async start() {
     await this.sdk.start();
+  }
+
+  getLoggerProvider() {
+    return this.loggerProvider;
+  }
+
+  getServiceName() {
+    return this.serviceName;
   }
 }
 
--- wf-manager/src/main.ts
+++ wf-manager/src/main.ts
@@ -1,4 +1,5 @@
 import OtelSDK from '@shared/Tracing';
+import OtelLogger from '@shared/OtelLogger';
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
@@ -8,12 +9,15 @@ import { HttpStatus, ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   // Start the OpenTelemetry SDK
   const serviceName = process.env.SERVICE_NAME!;
-  const signozUrl = process.env.SIGNOZ_URL!;
-  const otelSdk = new OtelSDK(serviceName, signozUrl);
+  const loggerUrl = process.env.LOGGER_URL!;
+  const traceUrl = process.env.TRACE_URL!;
+  const otelSdk = new OtelSDK(serviceName, loggerUrl, traceUrl);
   await otelSdk.start();
 
   // Create the NestJS application
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    logger: new OtelLogger(otelSdk),
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       errorHttpStatusCode: HttpStatus.BAD_REQUEST,
